924e8ba16b5a63c68124e4d1d8d7b2e4a7e35626
Zeljko Sokolovic <zeljko.sokolovic@zebracon.rs>
2023-01-16 11:01:18 +0100

    download xlsx functionality

--- XLSX to CSV/Controllers/HomeController.cs
+++ XLSX to CSV/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -25,6 +27,38 @@ namespace XLSX_to_CSV.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public void DownloadXLSX()
+        {
+            using (var client = new WebClient())
+            {
+                try
+                {
+                    byte[] filedata = client.DownloadData("https://bakerhughesrigcount.gcs-web.com/static-files/b562fc2a-b229-41eb-8407-54dda5dc7295");
+
+                    using (Stream ms = new MemoryStream(filedata))
+                    {
+                        using (var localStream = System.IO.File.Create("C:\\rigCounts.xlsx"))
+                        {
+                            byte[] buffer = new byte[1024];
+                            int bytesRead;
+
+                            do
+                            {
+                                bytesRead = ms.Read(buffer, 0, buffer.Length);
+                                localStream.Write(buffer, 0, bytesRead);
+
+                            } while (bytesRead > 0);
+
+                            localStream.Flush();
+                        }
+                    }
+                }
+                catch (System.Net.WebException)
+                {
+                    Console.WriteLine("Ne moze da kreira SSL tunel ka podacima.");
+                }
+            }
         }
     }
 }
